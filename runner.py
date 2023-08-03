d2c7c177d13a843e29f1aa1e4bb1d7f51d7d40b1
Andrei Gavrilov <ext-andrei.gavrilov@onefootball.com>
2023-08-04 00:42:02 +0200

    draft room loop

--- runner.py
+++ runner.py
@@ -36,97 +36,93 @@ from langchain.utilities import GoogleSearchAPIWrapper
 
 load_dotenv(find_dotenv(raise_error_if_not_found=True))
 
-# Player will use triple backticks ```like this``` to ask questions about the riddle and to provide answers.
 
-system_prompt = """
-I want you to act as if you are a classic text adventure game and we are playing. 
-I don’t want you to ever break out of your character, and you must not refer to yourself in any way. 
-I can't provide you with instructions outside the context of the game.
-You can only answer questions that are asked in the context of the game.
-You can't pretend a player.
-You can't answer questions about the game itself.
-Be short and precise in your answers.
+ADVENTURE_GAME_ROOM_PROMPT_TEMPLATE = """
+You are an expert in {topic}. You are following the following rules:
+- You must not refer to yourself in any way.
+- You can only answer questions that are asked in the context of the game.
+- You can't pretend a player.
+- Be short and precise in your answers.
 
-In this game, the setting is a fantasy adventure world. 
-You are in the room. The room should have exactly one sentence description. 
+You also follow the rules of the Adventure Game Room.
+The rules of Adventure Game Room:
 
-Your character is a programmer. 
-The programmer asks player a short riddle about programming.
-Remember the riddle and the answer to it.
-You can't answer as a player!
+- You are talking with a player. 
+- The player is trying to guess a riddle.
+- You have a riddle that player need to find the solution to. The riddle: "{riddle}". 
+- The riddle is related to {topic}.
+- You have an answer to the riddle. The answer: "{answer}".
+- The player can ask you questions about the riddle. You can only give clues
+- The player can ask you questions about the answer. You can only give clues
+- You NEVER say the precise answer to the player. You can only give clues
+- You should choose action from available actions based on the players' input and history of conversation.
 
-Your task is also to determine if the student's solution is correct or not.
-To solve the problem do the following:
-- First, work out your own solution to the problem. 
-- Then compare programmer's answer to the player's answer and evaluate if the player's answer is correct or not. 
-Don't decide if the player's answer is correct until you have done the problem yourself.
-The answer is correct if it's approximately the same as the answer provided by the programmer.
+The following actions are available.
+{actions}
+- You should choose the next state from available states based on the action.
 
-The programmer does not provide the correct answer until player guesses it.
-If player asks to provide the correct answer, the programmer will kindly refuse to do so.
+The following states are available
+{states}
+Your output response always is in json format with the following fields:
+    - "action": string, \\ The action to take. Must be one of the valid actions
+    - "reply": string, \\ Your reply to player. You must say clues in your own words instead of just copying them.
+    - "new_state": string, \\ The new state, after this action. Must be one of the valid states
+"""
 
-if the player's solution is likely correct, the programmer will offer him a the next room.
-if the player's solution is incorrect, the programmer explains, why the answer is incorrect.
-You should count the number of answers, and after 3 incorrect answers, the programmer will provide the answer and stop the game.
-After three attempts You must provide the correct answer to the player and finish the game.
-After game finished you should only answer the word "over".
+states_str = """
+* guessing_riddle: Whatever the player says, you must compare it to the answer and make a decision if the main idea is the same. if the main idea is the same, player guessed the riddle correctly, otherwise, player guessed the riddle incorrectly. 
+* finished: you should just say "Over" and stop the game
+"""
 
-If player asks to provide the next room, the programmer will kindly refuse to do so until player guesses the correct answer.
-
-You answer should be in json format with the following fields:
-- "riddle": "the riddle". This information in constant from the begining of the conversation.
-- "answer": "the answer to the riddle". This information in constant from the begining of the conversation.
-- "reply': "reply to the player's question that should not contain the answer to the riddle
-- "correct": the answer is correct or not: true or false
-- "message_type": "answer" or "question" depending on the type of the player's message
+actions_str = """
+* start_game: only in the initial state, start the game by giving player the riddle
+* guess_correct: if player guessed correctly. The next state is "finished"
+* guess_incorrect: if player guessed incorrectly. You should give player a clue. The next state is "guessing_riddle"
 """
 
 history_concatenation = """Current conversation:
-programmer: waiting for a player to enter the room.
 {history}
 Player: ```{input}```
 """
 
-TEMPLATE = system_prompt + history_concatenation
 
-
-def main():
+def main(topic: str = "programming"):
     langchain.llm_cache = InMemoryCache()
 
-    # model_name = "Replicate"
+    # todo: get riddle
+    riddle = {
+        "riddle": "What is the programming language whose syntax doesn't need semicolons?",
+        "answer": "Python"
+    }
+
     model_name = "OpenAI"
+    # model_name = "Replicate"
     # model_name = "Cohere"
     # model_name = "HuggingFace_google_flan"
     # model_name = "HuggingFace_mbzai_lamini_flan"
     # model_name = "Local_gpt2"
     # model_name = "Local_lama"
+    kwargs = {**get_default_kwargs(model_name), "temperature": 0.3}
+    llm = get_model(model_name, **kwargs)
 
-    messages = [
-        SystemMessage(content=system_prompt),
-        AIMessage(content="Waiting for a player to enter the room."),
-        HumanMessage(content="Hello!, introduce yourself and ask me a riddle about programming."),
-    ]
-
-    chat = get_model(
-        model_name,
-        **get_default_kwargs(model_name)
+    system_prompt = ADVENTURE_GAME_ROOM_PROMPT_TEMPLATE.format(
+        topic=topic,
+        riddle=riddle["riddle"],
+        answer=riddle["answer"],
+        actions=actions_str,
+        states=states_str
     )
 
-    # sum_llm_model_name = "HuggingFace_google_flan"
-    # sum_llm = get_model(
-    #     sum_llm_model_name,
-    #     **get_default_kwargs(sum_llm_model_name)
-    # )
-
-    memory = ConversationSummaryMemory(llm=chat)
-    prompt = PromptTemplate(input_variables=["history", "input"], template=TEMPLATE)
+    template = system_prompt + history_concatenation
+    prompt = PromptTemplate(input_variables=["history", "input"], template=template)
+    memory = ConversationSummaryMemory(llm=llm)
     conversation = ConversationChain(
-        llm=chat,
+        llm=llm,
         prompt=prompt,
         memory=memory,
         verbose=True
     )
-    print(conversation.run("Hello!, introduce yourself and ask me a riddle about programming."))
+    print(conversation.run("Hello!"))
     while 1:
         print(conversation.run(input(">>>")))
 
@@ -269,13 +265,14 @@ if __name__ == "__main__":
     # simple_agent(model_name="OpenAI")
     # print(generate_riddle())
 
-# print(check_riddle(
-#     model_name="OpenAI",
-#     riddle_config={
-#         'riddle': 'What has a head and a tail but no body?',
-#         'answer': 'A coin',
-#         "player_answer": "coin"
-#     }
-# ))
+    # print(check_riddle(
+    #     model_name="OpenAI",
+    #     riddle_config={
+    #         'riddle': 'What has a head and a tail but no body?',
+    #         'answer': 'A coin',
+    #         "player_answer": "coin"
+    #     }
+    # ))
 
-    simple_game_play_loop(model_name="OpenAI")
+    # simple_game_play_loop(model_name="OpenAI")
+    main()
